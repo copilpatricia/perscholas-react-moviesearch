b8ed53d9bd6dad0a757aca0db70e8e13c6d3c072
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-01-31 07:48:04 -0500

    upload lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
 function App() {
+  const apiKey = "1b4e047c";
+
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (searchTerm) => {
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovie(data);
+    } catch (error) {
+      console.error(error)
+    }
+  };
+
+  useEffect(() => {
+    getMovie("Godfather");
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }
 
 export default App;
+
+//movie[(Math.floor(Math.random() * movie.length))]
---
+++ src/components/Form.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+function Form({ moviesearch }) {
+  console.log(moviesearch);
+  const [formData, setFormData] = useState({
+    searchterm: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    moviesearch(formData.searchterm);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="searchterm"
+          onChange={handleChange}
+          value={formData.searchterm}
+        />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
+}
+
+export default Form;
